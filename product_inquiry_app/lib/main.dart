7eb1e5140b7d12eb556c0c7252c8c6dcd79130dc
ibrahimAskar2 <ibrahim.askkar@gmail.com>
2025-05-24 19:12:22 +0300

    d

--- product_inquiry_app/lib/main.dart
+++ product_inquiry_app/lib/main.dart
@@ -5,50 +5,52 @@ void main() {
   runApp(MaterialApp(
     title: 'استعلامات المواد',
     theme: ThemeData(primarySwatch: Colors.blue),
-    home: ProductInquiryScreen(),
+    home: ProductSearchScreen(),
   ));
 }
 
-class ProductInquiryScreen extends StatefulWidget {
-  @override
-  _ProductInquiryScreenState createState() => _ProductInquiryScreenState();
-}
-
-class _ProductInquiryScreenState extends State<ProductInquiryScreen> {
-  final TextEditingController _controller = TextEditingController();
-  String searchResult = '';
-
-  void search() {
-    setState(() {
-      searchResult = _controller.text.isEmpty ? '' : 'نتائج البحث عن: ${_controller.text}';
-    });
-  }
+class ProductSearchScreen extends StatelessWidget {
+  final List<String> dummyProducts = [
+    'مادة ١', 'مادة ٢', 'مادة ٣', 'مادة ٤'
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: TextDirection.rtl,
-      child: Scaffold(
-        appBar: AppBar(title: Text('استعلامات المواد')),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: _controller,
-                decoration: InputDecoration(
-                  labelText: 'ابحث عن المادة',
-                  suffixIcon: IconButton(
-                    icon: Icon(Icons.search),
-                    onPressed: search,
-                  ),
+    return Scaffold(
+      appBar: AppBar(title: Text('استعلامات المواد')),
+      body: ListView.builder(
+        itemCount: dummyProducts.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(dummyProducts[index]),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      ProductDetailScreen(productName: dummyProducts[index]),
                 ),
-              ),
-              SizedBox(height: 20),
-              Text(searchResult, style: TextStyle(fontSize: 18)),
-            ],
-          ),
-        ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ProductDetailScreen extends StatelessWidget {
+  final String productName;
+
+  const ProductDetailScreen({Key? key, required this.productName}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(productName)),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text('تفاصيل المنتج: $productName\nالأسعار والكمية حسب الفروع...'),
       ),
     );
   }
